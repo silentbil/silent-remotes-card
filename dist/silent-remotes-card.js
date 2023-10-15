d3e41522b2fe79f3f668caa1c4a348a83b478570
silentbil <natanb87@gmail.com>
2023-10-15 10:56:51 +0300

    fix lit import

--- dist/silent-remotes-card.js
+++ dist/silent-remotes-card.js
@@ -5,8 +5,8 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-import { customElement, } from 'https://cdn.skypack.dev/lit/decorators.js';
-import { css, html, LitElement } from 'https://cdn.skypack.dev/lit';
+import { customElement, } from 'https://cdn.skypack.dev/-/lit@v2.0.0-B36tAUEdI9Ino7UGfR7h/dist=es2019,mode=imports/optimized/lit/decorators.js';
+import { css, html, LitElement } from 'https://cdn.skypack.dev/-/lit@v2.0.0-B36tAUEdI9Ino7UGfR7h/dist=es2019,mode=imports/optimized/lit.js';
 function loadCSS(url) {
     const link = document.createElement('link');
     link.type = 'text/css';
